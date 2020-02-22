6d5feb18c6bdd2730d4b0835c03277491064777b
Dmitrij Kozin <dev.dim@koziny.ru>
2020-02-22 18:34:35 +0300

    refactor: перенос конфигурации модульных тестов

--- build/test.cmd
+++ build/test.cmd
@@ -11,7 +11,7 @@ IF NOT DEFINED _1CE_TargetDBConnection (
 pushd %ROOT_DIR%
 
 SET VANESSA_featurepath=%_1CE_TestUnitDir%
-call .\3dparty\1cv8 ENTERPRISE /WA- /DisableStartupDialogs /IBConnectionString %_1CE_TargetDBConnection% /Execute .\3dparty\vanessa-automation-single.epf /C"StartFeaturePlayer;workspaceRoot=%CD%;VBParams=%CD%\test\vanessa-conf.json"
+call .\3dparty\1cv8 ENTERPRISE /WA- /DisableStartupDialogs /IBConnectionString %_1CE_TargetDBConnection% /Execute .\3dparty\vanessa-automation-single.epf /C"StartFeaturePlayer;workspaceRoot=%CD%;VBParams=%CD%\test\unit\vanessa-conf.json"
 IF ERRORLEVEL 1 (
   popd
   exit /b %ERRORLEVEL%
